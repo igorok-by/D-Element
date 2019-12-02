87709e7b9f612c991917f2195c255480167249df
Igor <igorko.by@gmail.com>
2019-12-02 13:13:30 +0300

    Finish project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// BOOTSTRAP
+// import 'bootstrap/dist/js/bootstrap'
+
+// JQUERY
+import $ from 'jquery';
+global.jQuery = $;
+global.$ = $;
+
+// JS
+import './js/'
+
+// SCSS
+import './assets/scss/main.scss'
+
+// CSS (example)
+// import './assets/css/main.css'
